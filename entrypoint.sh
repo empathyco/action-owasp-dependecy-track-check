e91e18a4486bf1a1c816bde1307adb338e499d31
alvarodE <72568818+alvarodE@users.noreply.github.com>
2024-12-11 15:48:09 +0100

    Support npm & pnpm (with node@18)

--- entrypoint.sh
+++ entrypoint.sh
@@ -114,6 +114,35 @@ python() {
     upload_bom "bom.json" "."
 }
 
+npm() {
+    echo "[*]  Processing npm BoM"
+    curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+    apt-get install -y nodejs
+    npm install
+    npm audit fix --force
+    if [ ! $? = 0 ]; then
+        echo "[-] Error executing npm install. Stopping the action!"
+        exit 1
+    fi
+    npx --yes cyclonedx-bom -o bom.xml
+    upload_bom "bom.xml" "."
+}
+
+pnpm() {
+    echo "[*]  Processing pnpm BoM"
+    curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+    apt-get install -y nodejs
+    npm install -g pnpm
+    pnpm install
+    pnpm audit --fix
+    if [ ! $? = 0 ]; then
+        echo "[-] Error executing pnpm install. Stopping the action!"
+        exit 1
+    fi
+    npx --yes cyclonedx-bom -o bom.xml
+    upload_bom "bom.xml" "."
+}
+
 java
 
 case $LANGUAGE in
@@ -123,6 +152,14 @@ case $LANGUAGE in
 
 "python")
     python
+    ;;
+*)
+"npm")
+    npm
+    ;;
+*)
+"pnpm")
+    pnpm
     ;;
 *)
     echo "[-] Unsupported language: $LANGUAGE"
